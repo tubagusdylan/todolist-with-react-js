519ddcf0ec52f81efd9af653174c2abb816b34f2
tubagusdylan <tubagus.dylanr@gmail.com>
2022-05-13 10:35:27 +0700

    make the list better

--- src/components/InputToDo.js
+++ src/components/InputToDo.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ListToDo from "./ListToDo";
+import { BsPlus, BsCheck2, BsX } from "react-icons/bs";
 
 const InputToDo = (props) => {
   const [activity, setActivity] = useState("");
@@ -62,23 +63,29 @@ const InputToDo = (props) => {
       <form onSubmit={saveToDo}>
         <label className="label-input">Mau ngapain hari ini</label>
         {message && <div className="message">{message}</div>}
-        <input
-          type="text"
-          placeholder="Ketik di sini"
-          className="input"
-          value={activity}
-          onChange={(event) => {
-            setActivity(event.target.value);
-          }}
-        />
-        <button type="submit" className="button-1">
-          {edit.id ? "Save" : "Add"}
-        </button>
-        {edit.id && (
-          <button onClick={cancelEdit} className="button-1">
-            Cancel
-          </button>
-        )}
+        <div className="inputContainer">
+          <div>
+            <input
+              type="text"
+              placeholder="Ketik di sini"
+              className="input"
+              value={activity}
+              onChange={(event) => {
+                setActivity(event.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <button type="submit" className="button-1">
+              {edit.id ? <BsCheck2 className="icon" /> : <BsPlus className="icon" />}
+            </button>
+            {edit.id && (
+              <button onClick={cancelEdit} className="button-1">
+                <BsX className="icon" />
+              </button>
+            )}
+          </div>
+        </div>
       </form>
       {props.dataToDo.length > 0 ? (
         <ListToDo dataToDo={props.dataToDo} deleteToDo={props.deleteToDo} editToDo={editToDo} cancelEdit={cancelEdit} edit={edit} doneTodo={props.eventDoneTodo} />
--- src/components/ListToDo.js
+++ src/components/ListToDo.js
@@ -1,3 +1,5 @@
+import { BsXLg, BsPencil } from "react-icons/bs";
+
 const ListToDo = (props) => {
   const removeToDo = (todoId) => {
     const filteredTodos = props.dataToDo.filter((todo) => {
@@ -26,18 +28,28 @@ const ListToDo = (props) => {
   };
 
   return (
-    <ul>
+    <ul className="listContainer">
       {props.dataToDo.map((todo) => {
         return (
           <li key={todo.id}>
-            <input type="checkbox" onChange={doneTodoHandler.bind(this, todo)} checked={todo.done} />
-            {todo.done ? <s style={{ opacity: 0.5 }}>{todo.activity}</s> : <label>{todo.activity}</label>}
-            <button className="deleteButton" onClick={props.editToDo.bind(this, todo)}>
-              Edit
-            </button>
-            <button className="deleteButton" onClick={removeToDo.bind(this, todo.id)}>
-              Delete
-            </button>
+            <div className="listDescription">
+              <input type="checkbox" className="checkbox" onChange={doneTodoHandler.bind(this, todo)} checked={todo.done} />
+              {todo.done ? (
+                <s className="listLabel" style={{ opacity: 0.5 }}>
+                  {todo.activity}
+                </s>
+              ) : (
+                <label className="listLabel">{todo.activity}</label>
+              )}
+            </div>
+            <div>
+              <button className="deleteButton" onClick={props.editToDo.bind(this, todo)}>
+                <BsPencil className="icon" />
+              </button>
+              <button className="deleteButton" onClick={removeToDo.bind(this, todo.id)}>
+                <BsXLg className="icon" />
+              </button>
+            </div>
           </li>
         );
       })}
